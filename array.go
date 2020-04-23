14e3d59295e31bb5e1a22ab2abf2f5ea6f0bca11
clc <chenlc@youshu.cc>
2020-04-23 16:25:50 +0800

    cherry
    # Conflicts:
    #	array.go

--- array.go
+++ array.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func main()  {
-	var n [10] int
-	var i,j int
+func main() {
+	var n [10]int
+	var i, j int
 
 	/* 为数组n初始化元素 */
 	for i = 0; i < 11; i++ {
@@ -12,6 +12,10 @@ func main()  {
 	}
 
 	for j = 0; j < 10; j++ {
-		fmt.Printf("Element[%d] = %d\n", j, n[j]);
+		fmt.Printf("Element[%d] = %d\n", j, n[j])
 	}
 }
+
+/**
+array的注释
+*/
